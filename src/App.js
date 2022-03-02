b0e5675016c5f8da1e50a744daa37dbe53e5f874
Atul Bansal <atulbansal9@gmail.com>
2022-03-02 13:20:31 +0530

    mui css typography

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Typography variant='h1'>h1 Hadding</Typography>
+      <Typography variant='h2'>h2 Hadding</Typography>
+      <Typography variant='h3'>h3 Hadding</Typography>
+      <Typography variant='h4'>h4 Hadding</Typography>
+      <Typography variant='h5'>h5 Hadding</Typography>
+      <Typography variant='h6'>h6 Hadding</Typography>
+      <Typography variant='subtitle1'>subtitle1 Hadding</Typography>
+      <Typography variant='subtitle2'>subtitle2 Hadding</Typography>
+      <Typography variant='body1'>span1 Hadding</Typography>
+      <Typography variant='body2'>span2 Hadding</Typography>
+    </>
+  )
 }
 
-export default App;
+export default App
